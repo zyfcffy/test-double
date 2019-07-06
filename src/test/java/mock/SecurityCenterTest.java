8344d0922886fba7a187832e1947acf865334d80
bingwang <bingwang@thoughtworks.com>
2019-07-06 18:18:18 +0800

    [王冰]change to junit5

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,14 +1,15 @@
 package mock;
 
-import org.junit.Before;
-import org.junit.Test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
 
-    @Before
+    @BeforeEach
     public void setUp() {
     }
 
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,6 +1,7 @@
 package stub;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 public class GradeServiceTest {
     /* 需求描述：
