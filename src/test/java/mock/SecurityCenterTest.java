1706ea286a51346182e46f9af93a7f09b789623f
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-15 20:05:21 +0800

    [FuFangyuan]feat:finished mockito exerise

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -3,17 +3,25 @@ package mock;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
-    /* 需求描述：
-    编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
-    * */
-
-    @BeforeEach
-    public void setUp() {
-    }
+    @Mock
+    private DoorPanel mockDoorPanel;
+    @InjectMocks
+    private SecurityCenter securityCenter;
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+        verify(mockDoorPanel,times(1)).close();
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -2,14 +2,30 @@ package stub;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
-    /* 需求描述：
-    编写GradeService类的单元测试，单元测试calculateAverageGrades方法
-    * */
+
+    @Mock
+    GradeSystem gradeSystem;
+    @InjectMocks
+    GradeService mockGradeService;
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        when(gradeSystem.gradesFor(1)).thenReturn(Arrays.asList(80.0,90.0,100.0));
+        double result = mockGradeService.calculateAverageGrades(1);
+        assertEquals(90,result);
     }
 }
